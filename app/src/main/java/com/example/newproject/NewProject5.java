bb162473357251b720c4d4d0bf481f3bacb2dc88
americandev <kubadaev48@gmail.com>
2021-04-20 20:17:08 +0600

    mkmkm

---
+++ app/src/main/java/com/example/newproject/NewProject5.java
@@ -0,0 +1,14 @@
+package com.example.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewProject5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_project5);
+    }
+}
